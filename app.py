a6fd64029464de3dc5811907514b45cf09d84b59
Syed Akmal Abbas <akmal650iiui@gmail.com>
2023-01-09 18:20:03 +0500

    Dataframe

--- app.py
+++ app.py
@@ -160,7 +160,11 @@ with placeholder.container():
     st.markdown("### Detailed Data View")
     # status_filter = st.sidebar.selectbox("View Dashboard According to", pd.unique(df["claimed"]))
 
-    st.dataframe(df)
+
+    status_filter = st.selectbox("Select status to view the data", pd.unique(df["status"]))
+    # dataframe filter
+    df_refined = df[df["status"] == status_filter]
+    st.dataframe(df_refined)
 
     # fig_col3, fig_col4 = st.columns(2)
     # # color_discrete_map = {"Removed": 'red', "Accepted": "green"}
